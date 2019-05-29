0ba96342b139b3f049ae041b2910b050d55c9b6b
Aaron <pinatamostgrim@gmail.com>
2019-05-29 06:50:13 -0300

    Log training_wrapper's output to file
    The log directory is defined in settings.py. In the future, an optional argument will allow redirecting the log file elsewhere.
    
    Additionally, current working directory when executing training is set to the project's root folder. This allows us to use project relative paths more easily, which are easier to understand, adjust, and parse.

--- grim-agents/grimagents/settings.py
+++ grim-agents/grimagents/settings.py
@@ -3,10 +3,24 @@
 from pathlib import Path
 
 
+# This is a relative path to the project's root folder from setting.py's location
 _PROJECT_ROOT_RELATIVE_PATH = '..\\..'
 
+# This is a relative path to the grim-agents log folder
+_LOG_FOLDER_PATH = 'grim-agents\\logs'
 
-def get_project_root_folder():
+
+def get_project_folder_absolute():
     """Returns the absolute path for the project's root folder."""
     relative_path = Path(__file__).parent / _PROJECT_ROOT_RELATIVE_PATH
     return relative_path.resolve()
+
+
+def get_log_folder():
+    """Returns log folder's path relative to the project's root folder."""
+    return Path(_LOG_FOLDER_PATH)
+
+
+def get_log_folder_absolute():
+    """Returns log folder as an absolute path"""
+    return get_project_folder_absolute() / get_log_folder()
--- grim-agents/grimagents/training_wrapper.py
+++ grim-agents/grimagents/training_wrapper.py
@@ -1,47 +1,56 @@
 #!/usr/bin/env python3
-"""CLI application that wraps 'mlagents-learn' and facilitates training logging."""
+"""CLI application that wraps 'mlagents-learn' and facilitates training logging.
 
 # - use command_util to launch an mlagents-learn process
 # - log output to file
 # - record the runtime of the training
 # - parse args for '--run-id'
+"""
+
 
 import argparse
-import time
+from datetime import datetime
 import sys
+import time
 
-from argparse import Namespace
 from pathlib import Path
 from subprocess import Popen, PIPE
 import settings as settings
 
 
-_TRAINER_RELATIVE_PATH = '..\\mock_trainer.py'
+_TRAINER_RELATIVE_PATH = 'grim-agents\\mock_trainer.py'
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 
 def main():
-    # args = parse_args(sys.argv[1:])
-    # log_path = Path(args.log_path)
-    # run_id = args.run_id
+    args = parse_args(sys.argv[1:])
 
-    print(settings.get_project_root_folder())
-    print(type(settings.get_project_root_folder()))
-    return
+    cwd = settings.get_project_folder_absolute()
+    trainer_path = _TRAINER_RELATIVE_PATH
 
-    trainer_path = Path(__file__).parent / _TRAINER_RELATIVE_PATH
-    log_path = Path('mock_training.log')
+    log_folder = settings.get_log_folder_absolute()
+    if not log_folder.exists():
+        log_folder.mkdir(parents=True, exist_ok=True)
+
+    run_id = Path(args.run_id)
+    if not run_id.suffix == '.log':
+        run_id = run_id.with_suffix('.log')
+
+    log_path = log_folder / run_id.name
 
     # Note: As these arguments are being passed direction into popen,
     # the trainer path does not need to be enclosed in quotes to support
     # paths with spaces in them.
-
-    # Note: We use absolute path here for the trainer script as we
-    # do not know what the current working directory will be.
-    command = ['pipenv', 'run', 'python', f"{trainer_path.resolve()}"] + sys.argv[1:]
+    command = ['pipenv', 'run', 'python', f"{trainer_path}"] + sys.argv[1:]
     print(' '.join(command))
 
-    with Popen(command, stdout=PIPE, bufsize=1, universal_newlines=True) as p, \
-            log_path.open('w') as f:
+    with Popen(
+        command, stdout=PIPE, cwd=cwd, bufsize=1, universal_newlines=True
+    ) as p, log_path.open('a') as f:
+
+        started = f'Training started at {datetime.now().strftime(TIME_FORMAT)}'
+        print(started)
+        f.write(started)
 
         start_time = time.perf_counter()
 
@@ -50,7 +59,10 @@ def main():
             f.write(line)
 
         end_time = time.perf_counter()
-        print(f'Training finished in {end_time - start_time} seconds.')
+
+        finished = f'Training finished at {datetime.now().strftime(TIME_FORMAT)} ({end_time - start_time} seconds)\n\n'
+        print(finished)
+        f.write(finished)
 
     print('━━━━━━━━━━━━━━━━━━━━━━━━━━')
     print(p.returncode)
@@ -59,10 +71,11 @@ def main():
 def parse_args(argv):
     parser = argparse.ArgumentParser(
         prog='training_wrapper',
-        description='CLI application that wraps mlagents-learn with logging')
+        description='CLI application that wraps mlagents-learn with logging',
+    )
 
-    parser.add_argument('--log-path', type=str, help='Log-path help')
-    parser.add_argument('--run-id', type=str, help='Run-id help')
+    # parser.add_argument('--log-path', type=str, help='Log-path help')
+    parser.add_argument('--run-id', type=str, help='Run-id help', required=True)
 
     args = parser.parse_args(argv)
     return args
