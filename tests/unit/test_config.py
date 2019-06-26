96d0ab357a876cc87e57b507aa8545056b688266
Aaron <pinatamostgrim@gmail.com>
2019-06-26 06:11:21 -0300

    Add unit test for loading grimagent configuration files

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -7,6 +7,16 @@ import grimagents.config as config
 
 GRIM_CONFIG_FILE = 'grim_config.json'
 
+VALID_CONFIGURATION = {
+    "trainer-config-path": "config\\3DBall.yaml",
+    "--run-id": "3DBall",
+}
+
+INVALID_CONFIGURATION = {
+    "--env": "builds\\3DBall\\Unity Environment.exe",
+    "--run-id": "3DBall",
+}
+
 
 def get_grim_config_file_path():
     return Path(__file__).parent / GRIM_CONFIG_FILE
@@ -36,5 +46,29 @@ def test_create_grim_config_file(fixture_grim_config_file):
         file_data = json.load(f)
 
     assert data == file_data
+
+
+def test_load_grim_config_file(fixture_grim_config_file):
+    """Test for loading a valid grimagents configuration file."""
+
+    path = get_grim_config_file_path()
+    with path.open(mode='w') as f:
+        json.dump(VALID_CONFIGURATION, f, indent=4)
+
+    file_data = config.load_config_file(path)
+    assert file_data == VALID_CONFIGURATION
+
+
+def test_invalid_configuration_error(fixture_grim_config_file):
+    """Test for raising an InvalidConfigurationError for an invalid grimagents
+    configuration file.
+    """
+
+    path = get_grim_config_file_path()
+    with path.open(mode='w') as f:
+        json.dump(INVALID_CONFIGURATION, f, indent=4)
+
+    with pytest.raises(config.InvalidConfigurationError):
+        config.load_config_file(path)
 
 
