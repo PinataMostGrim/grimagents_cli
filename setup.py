45e2cb717689f36ed584b7afe09d23f4a59a204f
Aaron <pinatamostgrim@gmail.com>
2019-07-30 14:51:38 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("readme.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="grimagents",
+    version="0.3.0",
+    description="Collection of command line applications that wrap Unity Machine Learning Agents with quality of life improvements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PinataMostGrim/grimagents_cli",
+    author="PinataMostGrim",
+    author_email="pinatamostgrim@gmail.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pyyaml"
+    ],
+    python_requires=">=3.6,<3.7"
+)
