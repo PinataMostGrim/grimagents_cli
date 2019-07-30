be908adb49d1af6dc5fa233c8b1f1286a8e14106
Aaron <pinatamostgrim@gmail.com>
2019-07-30 15:53:28 -0300

    Update convenience batch files to execute CLIs using pipenv
    This requires the package be installed from local setup.py using 'pipenv install -e grim-agents'. This ultimately makes less assumptions about where grimagents is installed and where the virtual environment is located. 
    
    One problem this change does introduce is that if these batch files are executed from a directory where a virtual environment has not been setup using pipenv, pipenv will begin installing one. This can potentially be eliminated using if conditionals in the batch files.

--- search.bat
+++ search.bat
@@ -1,11 +1,4 @@
 @echo off
-:: Run grimagents.search using a virtual environment.
+:: Run grimagents.search using Pipenv
 
-:: In order to support executing this batch file from from any location,
-:: the module's parent folder must be added to the Python path.
-
-:: NOTE: By default, this file must be copied or moved up one folder for the grimagents folder to be added to PYTHONPATH.
-setlocal
-set PYTHONPATH=%~dp0grim-agents
-"%~dp0.venv\Scripts\python.exe" "%~dp0grim-agents\grimagents\search.py" %*
-endlocal
+pipenv run python -m grimagents.search %*
