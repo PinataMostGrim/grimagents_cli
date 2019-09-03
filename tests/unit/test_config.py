31e659750f3f3a051216139d54850b21770c872a
Aaron <pinatamostgrim@gmail.com>
2019-09-03 18:24:01 -0300

    Refactor test_config.py to use more fixtures for clarity

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -31,7 +31,7 @@ TRAINER_CONFIG = {
         'num_epoch': 3,
         'num_layers': 2,
         'time_horizon': 64,
-        'sequence_length': 64
+        'sequence_length': 64,
     }
 }
 
@@ -54,62 +54,74 @@ def delete_file(file: Path):
 
 
 @pytest.fixture
-def fixture_grim_config_file():
+def grim_config():
+    return get_grim_config_file_path()
+
+
+@pytest.fixture
+def trainer_config():
+    return get_trainer_config_file_path()
+
+
+@pytest.fixture
+def curriculum_config():
+    return get_curriculum_file_path()
+
+
+@pytest.fixture
+def fixture_cleanup_grim_config():
     delete_file(get_grim_config_file_path())
-    yield 'fixture_grim_config_file'
+    yield 'fixture_cleanup_grim_config'
     delete_file(get_grim_config_file_path())
 
 
 @pytest.fixture
-def fixture_trainer_config_file():
+def fixture_cleanup_trainer_config():
     delete_file(get_trainer_config_file_path())
-    yield 'fixture_trainer_config_file'
+    yield 'fixture_cleanup_trainer_config'
     delete_file(get_trainer_config_file_path())
 
 
 @pytest.fixture
-def fixture_curriculum_file():
+def fixture_cleanup_curriculum():
     delete_file(get_curriculum_file_path())
-    yield 'fixture_curriculum_file'
+    yield 'fixture_cleanup_curriculum'
     delete_file(get_curriculum_file_path())
 
 
-def test_create_grim_config_file(fixture_grim_config_file):
+def test_create_grim_config_file(grim_config, fixture_cleanup_grim_config):
     """Test for creating a new grimagents configuration file."""
 
-    path = get_grim_config_file_path()
-    config.create_grim_config_file(path)
-    assert path.exists()
+    config.create_grim_config_file(grim_config)
+    assert grim_config.exists()
 
     data = config.get_default_grim_config()
-    with path.open('r') as f:
+    with grim_config.open('r') as f:
         file_data = json.load(f)
 
     assert data == file_data
 
 
-def test_load_grim_config_file(fixture_grim_config_file):
+def test_load_grim_config_file(grim_config, fixture_cleanup_grim_config):
     """Test for loading a valid grimagents configuration file."""
 
-    path = get_grim_config_file_path()
-    with path.open(mode='w') as f:
+    with grim_config.open(mode='w') as f:
         json.dump(VALID_GRIM_CONFIG, f, indent=4)
 
-    file_data = config.load_grim_config_file(path)
+    file_data = config.load_grim_config_file(grim_config)
     assert file_data == VALID_GRIM_CONFIG
 
 
-def test_invalid_configuration_error(fixture_grim_config_file):
+def test_invalid_configuration_error(grim_config, fixture_cleanup_grim_config):
     """Test for raising an InvalidConfigurationError for an invalid grimagents
     configuration file.
     """
 
-    path = get_grim_config_file_path()
-    with path.open(mode='w') as f:
+    with grim_config.open(mode='w') as f:
         json.dump(INVALID_GRIM_CONFIG, f, indent=4)
 
     with pytest.raises(config.InvalidConfigurationError):
-        config.load_grim_config_file(path)
+        config.load_grim_config_file(grim_config)
 
 
 def test_configuration_validation():
@@ -125,34 +137,31 @@ def test_configuration_validation():
     assert config.validate_grim_configuration(configuration) is True
 
 
-def test_load_trainer_configuration(fixture_trainer_config_file):
+def test_load_trainer_configuration(trainer_config, fixture_cleanup_trainer_config):
     """Tests for the correct loading of a trainer configuration dictionary from file. """
 
-    path = get_trainer_config_file_path()
-    with path.open(mode='w') as f:
+    with trainer_config.open(mode='w') as f:
         yaml.dump(TRAINER_CONFIG, f, indent=4)
 
-    loaded_configuration = config.load_trainer_configuration(path)
+    loaded_configuration = config.load_trainer_configuration(trainer_config)
     assert loaded_configuration['default']['buffer_size'] == 10240
 
 
-def test_create_trainer_config_file(fixture_trainer_config_file):
+def test_create_trainer_config_file(trainer_config, fixture_cleanup_trainer_config):
     """Test for creating default trainer configuration files."""
 
-    path = get_trainer_config_file_path()
-    config.create_trainer_configuration_file(path)
-    assert path.exists()
+    config.create_trainer_configuration_file(trainer_config)
+    assert trainer_config.exists()
 
 
-def test_create_curriculum_file(fixture_curriculum_file):
+def test_create_curriculum_file(curriculum_config, fixture_cleanup_curriculum):
     """Test for creating a default curriculum file."""
 
-    path = get_curriculum_file_path()
-    config.create_curriculum_file(path)
-    assert path.exists()
+    config.create_curriculum_file(curriculum_config)
+    assert curriculum_config.exists()
 
 
-def test_no_add_search_entry(fixture_grim_config_file, monkeypatch):
+def test_no_add_search_entry(grim_config, fixture_cleanup_grim_config, monkeypatch):
     """Test for correct handling of the 'add_search' flag in config.edit_grim_config_file()
     """
 
@@ -161,22 +170,21 @@ def test_no_add_search_entry(fixture_grim_config_file, monkeypatch):
 
     monkeypatch.setattr(command_util, "open_file", mock_open_file)
 
-    path = get_grim_config_file_path()
-    config.edit_grim_config_file(path, add_search=False)
+    config.edit_grim_config_file(grim_config, add_search=False)
 
-    with path.open('r') as f:
+    with grim_config.open('r') as f:
         file_data = json.load(f)
 
     assert 'search' not in file_data
 
-    config.edit_grim_config_file(path, add_search=True)
-    with path.open('r') as f:
+    config.edit_grim_config_file(grim_config, add_search=True)
+    with grim_config.open('r') as f:
         file_data = json.load(f)
 
     assert 'search' in file_data
 
 
-def test_no_overwrite_search_entry(fixture_grim_config_file, monkeypatch):
+def test_no_overwrite_search_entry(grim_config, fixture_cleanup_grim_config, monkeypatch):
     """Tests to ensure config.edit_grim_config_file(add_search=True) does not overwrite an existing search entry.
     """
 
@@ -191,13 +199,12 @@ def test_no_overwrite_search_entry(fixture_grim_config_file, monkeypatch):
         "search": [],
     }
 
-    path = get_grim_config_file_path()
-    with path.open('w') as f:
+    with grim_config.open('w') as f:
         json.dump(configuration, f)
 
-    config.edit_grim_config_file(path, add_search=True)
+    config.edit_grim_config_file(grim_config, add_search=True)
 
-    with path.open('r') as f:
+    with grim_config.open('r') as f:
         file_data = json.load(f)
 
     assert 'brain' not in file_data['search']
