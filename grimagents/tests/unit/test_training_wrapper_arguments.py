4cb1a63ce1b7aa6a6fcaa20b099127b28424b9f1
Aaron <pinatamostgrim@gmail.com>
2020-06-07 17:06:50 -0300

    Restructure test_training_wrapper_arguments.py
    Break tests into more logical groupings. Changes include:
    - A test to check the handling of mandatory configuration values
    - A test to check the handling of all the optional configuration values
    - A test to check argument overrides correctly overwrite configuration values
    
    This should improve clarity about what is being tested where, and improve the ease of adding or updating training argument functionality in the future.

--- grimagents/tests/unit/test_training_wrapper_arguments.py
+++ grimagents/tests/unit/test_training_wrapper_arguments.py
@@ -11,13 +11,14 @@ from grimagents.training_commands import TrainingWrapperArguments
 def grim_config():
     return {
         'trainer-config-path': 'config/3DBall.yaml',
-        '--env': 'builds/3DBall/3DBall.exe',
+        '--run-id': '3DBall',
+        '--env': '',
         '--sampler': '',
-        '--export-path': 'UnitySDK/Assets/ML-Agents/Examples/3DBall/ImportedModels',
+        '--export-path': '',
         '--curriculum': '',
         '--keep-checkpoints': '',
         '--lesson': '',
-        '--run-id': '3DBall',
+        "--initialize-from": "",
         '--save-freq': '',
         '--seed': '',
         '--base-port': '',
@@ -37,12 +38,80 @@ def grim_config():
 
 
 def test_create_training_arguments(grim_config):
-    """Test for creating TrainingWrapperArguments with default configuration."""
+    """Test for creating TrainingWrapperArguments with mandatory configuration values."""
 
     arguments = TrainingWrapperArguments(grim_config)
 
     # The absolute path to training_wrapper.py will differ based on the system running this test.
     result = arguments.get_arguments()
+    result[3] = 'grimagents/training_wrapper.py'
+
+    # 'trainer-config-path' and '--run-id' should be present.
+    assert result == [
+        'pipenv',
+        'run',
+        'python',
+        'grimagents/training_wrapper.py',
+        'config/3DBall.yaml',
+        '--run-id',
+        '3DBall',
+    ]
+
+
+def test_optional_configuration_entries(grim_config):
+    """Tests the correct handling of optional configuration entries, including:
+        --base-port
+        --cpu
+        --curriculum
+        --debug
+        --docker-target-name
+        --env
+        --env-args
+        --export-path
+        --height
+        --keep-checkpoints
+        --lesson
+        --num-envs
+        --quality-level
+        --sampler
+        --save-freq
+        --seed
+        --target-frame-rate
+        --time-scale
+        --width
+
+    Arguments requiring special handling are tested elsewhere. Examples include:
+        --multi-gpu
+        --no-graphics
+        --timestamp
+        --inference
+    """
+
+    grim_config['--base-port'] = 5010
+    grim_config['--cpu'] = True
+    grim_config['--curriculum'] = 'curricula/3DBall.yaml'
+    grim_config['--debug'] = True
+    grim_config['--docker-target-name'] = 'unity-volume'
+    grim_config['--env'] = 'builds/3DBall/3DBall.exe'
+    grim_config['--env-args'] = ["--num-orcs", 42]
+    grim_config['--export-path'] = 'UnitySDK/Assets/ML-Agents/Examples/3DBall/ImportedModels'
+    grim_config['--height'] = 240
+    grim_config['--keep-checkpoints'] = 10
+    grim_config['--lesson'] = 2
+    grim_config['--num-envs'] = 5
+    grim_config['--quality-leve'] = 1
+    grim_config['--sampler'] = 'config/3DBall_randomize.yaml'
+    grim_config['--save-freq'] = 5000
+    grim_config['--seed'] = 500
+    grim_config['--run-id'] = '3DBall-2020-05-23_17-43-24'
+    grim_config['--target-frame-rate'] = 60
+    grim_config['--time-scale'] = 20
+    grim_config['--width'] = 320
+
+    arguments = TrainingWrapperArguments(grim_config)
+    result = arguments.get_arguments()
+
+    # The absolute path to training_wrapper.py will differ based on the system running this test.
     result[3] = 'grimagents/training_wrapper.py'
 
     assert result == [
@@ -51,17 +120,114 @@ def test_create_training_arguments(grim_config):
         'python',
         'grimagents/training_wrapper.py',
         'config/3DBall.yaml',
+        '--run-id',
+        '3DBall-2020-05-23_17-43-24',
         '--env',
         'builds/3DBall/3DBall.exe',
+        '--sampler',
+        'config/3DBall_randomize.yaml',
         '--export-path',
         'UnitySDK/Assets/ML-Agents/Examples/3DBall/ImportedModels',
+        '--curriculum',
+        'curricula/3DBall.yaml',
+        '--keep-checkpoints',
+        10,
+        '--lesson',
+        2,
+        '--save-freq',
+        5000,
+        '--seed',
+        500,
+        '--base-port',
+        5010,
+        '--num-envs',
+        5,
+        '--cpu',
+        '--width',
+        320,
+        '--height',
+        240,
+        '--time-scale',
+        20,
+        '--target-frame-rate',
+        60,
+        '--debug',
+        '--docker-target-name',
+        'unity-volume',
+        '--quality-leve',
+        1,
+        '--env-args',
+        '--num-orcs',
+        42,
+    ]
+
+
+def test_training_arguments_timestamp(monkeypatch, grim_config):
+    """Test TrainingWrapperArguments correctly applies a timestamp."""
+
+    def mock_return():
+        return '2019-06-29_17-13-41'
+
+    monkeypatch.setattr(grimagents.common, "get_timestamp", mock_return)
+
+    grim_config['--timestamp'] = True
+    arguments = TrainingWrapperArguments(grim_config)
+    arguments_string = arguments.get_arguments_as_string()
+
+    assert '--run-id 3DBall-2019-06-29_17-13-41' in arguments_string
+
+
+def test_training_arguments_handles_timestamp(grim_config):
+    """Test correct processing of the '--timestamp' argument."""
+
+    # '--timestamp' should be present
+    grim_config['--timestamp'] = True
+    arguments = TrainingWrapperArguments(grim_config)
+    assert '--timestamp' not in arguments.get_arguments()
+
+    # '--timestamp' should not be present
+    grim_config['--timestamp'] = False
+    arguments = TrainingWrapperArguments(grim_config)
+    assert '--timestamp' not in arguments.get_arguments()
+
+
+def test_training_arguments_inference(grim_config):
+    """Tests correct processing of the '--inference' argument.
+
+    - Ensures '--inference' argument is added
+    - Ensures get_arguments() can handle no additional args being set"
+    - Ensures the '--export-path' argument is removed
+    """
+
+    grim_config['--export-path'] = 'UnitySDK/Assets/ML-Agents/Examples/3DBall/ImportedModels'
+
+    # '--inference' should not be present
+    grim_config['--inference'] = False
+    arguments = TrainingWrapperArguments(grim_config)
+    assert '--inference' not in arguments.get_arguments()
+
+    # '--inference' should be present and '--export-path' argument should be removed
+    grim_config['--inference'] = True
+    arguments = TrainingWrapperArguments(grim_config)
+    result = arguments.get_arguments()
+
+    # The absolute path to training_wrapper.py will differ based on the system running this test.
+    result[3] = 'grimagents/training_wrapper.py'
+
+    assert result == [
+        'pipenv',
+        'run',
+        'python',
+        'grimagents/training_wrapper.py',
+        'config/3DBall.yaml',
         '--run-id',
         '3DBall',
+        '--inference',
     ]
 
 
 def test_training_arguments_handles_no_graphics(grim_config):
-    """Test for correct handling of the '--no-graphics' argument."""
+    """Test for correct processing of the '--no-graphics' argument."""
 
     # '--no-graphics' should be present
     grim_config['--no-graphics'] = True
@@ -75,7 +241,7 @@ def test_training_arguments_handles_no_graphics(grim_config):
 
 
 def test_training_arguments_handles_multi_gpu(grim_config):
-    """Test for correct handling of the '--multi-gpu' argument."""
+    """Test for correct processing of the '--multi-gpu' argument."""
 
     # '--multi-gpu' should be present
     grim_config['--multi-gpu'] = True
@@ -89,7 +255,7 @@ def test_training_arguments_handles_multi_gpu(grim_config):
 
 
 def test_training_arguments_handles_cpu(grim_config):
-    """Test for correct handling of the '--cpu' argument."""
+    """Test for correct processing of the '--cpu' argument."""
 
     # '--cpu' should be present
     grim_config['--cpu'] = True
@@ -102,22 +268,8 @@ def test_training_arguments_handles_cpu(grim_config):
     assert '--cpu' not in arguments.get_arguments()
 
 
-def test_training_arguments_excludes_timestamp(grim_config):
-    """Test for ensuring TrainingWrapperArguments excludes the '--timestamp' argument."""
-
-    # '--timestamp' should be present
-    grim_config['--timestamp'] = True
-    arguments = TrainingWrapperArguments(grim_config)
-    assert '--timestamp' not in arguments.get_arguments()
-
-    # '--timestamp' should not be present
-    grim_config['--timestamp'] = False
-    arguments = TrainingWrapperArguments(grim_config)
-    assert '--timestamp' not in arguments.get_arguments()
-
-
 def test_training_arguments_add_additional_args(grim_config):
-    """Test for TrainingWrapperArguments correctly setting additional arguments."""
+    """Test that TrainingWrapperArguments correctly appends additional arguments."""
 
     arguments = TrainingWrapperArguments(grim_config)
     additional_args = ['--load']
@@ -133,10 +285,6 @@ def test_training_arguments_add_additional_args(grim_config):
         'python',
         'grimagents/training_wrapper.py',
         'config/3DBall.yaml',
-        '--env',
-        'builds/3DBall/3DBall.exe',
-        '--export-path',
-        'UnitySDK/Assets/ML-Agents/Examples/3DBall/ImportedModels',
         '--run-id',
         '3DBall',
         '--load',
@@ -144,54 +292,46 @@ def test_training_arguments_add_additional_args(grim_config):
 
 
 def test_override_configuration_values(grim_config):
-    """Test for correct handling of TrainingWrapperArguments argument overrides.
-
-    Ensures the following arguments are overridden:
-        --trainer-config
+    """Test that TrainingWrapperArguments correctly applies argument overrides, including:
+        --base-port
         --env
-        --sampler
         --lesson
-        --run-id
-        --num-envs
-        --no-graphics
-        --timestamp
         --multi-gpu
-        --cpu
-        --width
-        --height
-        --time-scale
-        --quality-leve
-        --target-frame-rate
+        --no-graphics
+        --num-envs
+        --run-id
+        --sampler
+        --timestamp
+        --trainer-config
     """
 
+    grim_config['--base-port'] = 5010
+    grim_config['--env'] = 'builds/3DBall/3DBall.exe'
+    grim_config['--lesson'] = 1
+    grim_config['--multi-gpu'] = True
+    grim_config['--no-graphics'] = False
+    grim_config['--num-envs'] = 2
     grim_config['--sampler'] = 'config/3DBall_generalize.yaml'
     grim_config['--timestamp'] = True
-    grim_config['--multi-gpu'] = True
-    grim_config['--cpu'] = True
-    grim_config['--width'] = 320
-    grim_config['--height'] = 240
-    grim_config['--time-scale'] = 20
-    grim_config['--quality-leve'] = 1
-    grim_config['--target-frame-rate'] = 60
 
     override_args = Namespace(
+        args=[],
+        base_port=6010,
         configuration_file='config/3DBall_grimagents.json',
-        trainer_config='config/PushBlock_grimagents.json',
         env='builds/PushBlock/PushBlock.exe',
-        sampler='config/PushBlock_generalize.yaml',
-        resume=False,
-        lesson=2,
-        run_id='PushBlock',
-        base_port=5010,
-        num_envs=2,
-        inference=False,
         graphics=None,
-        no_graphics=True,
-        timestamp=None,
-        no_timestamp=True,
+        inference=False,
+        lesson=3,
         multi_gpu=None,
+        no_graphics=True,
         no_multi_gpu=True,
-        args=['--load'],
+        no_timestamp=True,
+        num_envs=4,
+        resume=False,
+        run_id='PushBlock',
+        sampler='config/PushBlock_generalize.yaml',
+        timestamp=None,
+        trainer_config='config/PushBlock_grimagents.json',
     )
 
     arguments = TrainingWrapperArguments(grim_config)
@@ -209,44 +349,32 @@ def test_override_configuration_values(grim_config):
         'python',
         'grimagents/training_wrapper.py',
         'config/PushBlock_grimagents.json',
+        '--run-id',
+        'PushBlock',
         '--env',
         'builds/PushBlock/PushBlock.exe',
         '--sampler',
         'config/PushBlock_generalize.yaml',
-        '--export-path',
-        'UnitySDK/Assets/ML-Agents/Examples/3DBall/ImportedModels',
         '--lesson',
-        '2',
-        '--run-id',
-        'PushBlock',
+        '3',
         '--base-port',
-        5010,
+        6010,
         '--num-envs',
-        '2',
+        '4',
         '--no-graphics',
-        '--cpu',
-        '--width',
-        320,
-        '--height',
-        240,
-        '--time-scale',
-        20,
-        '--target-frame-rate',
-        60,
-        '--quality-leve',
-        1,
-        '--load',
     ]
 
 
 def test_inference_override_configuration_values(grim_config):
     """Test for correct handling of the '--inference' argument override."""
 
-    # We test the override handling of the '--inference' argument by itself as it will affect the result of the '--env' argument.
+    # We test the override handling of the '--inference' argument by itself as it will affect the result of the '--export-path' argument.
+
+    grim_config['--export-path'] = 'UnitySDK/Assets/ML-Agents/Examples/3DBall/ImportedModels'
 
     override_args = Namespace(
         configuration_file='config/3DBall_grimagents.json',
-        trainer_config='config/PushBlock_grimagents.json',
+        trainer_config=None,
         resume=False,
         env=None,
         sampler=None,
@@ -272,14 +400,13 @@ def test_inference_override_configuration_values(grim_config):
     # The absolute path to training_wrapper.py will differ based on the system running this test.
     result[3] = 'grimagents/training_wrapper.py'
 
+    # The '--inference' argument should be present and '--export-path' should not.
     assert result == [
         'pipenv',
         'run',
         'python',
         'grimagents/training_wrapper.py',
-        'config/PushBlock_grimagents.json',
-        '--env',
-        'builds/3DBall/3DBall.exe',
+        'config/3DBall.yaml',
         '--run-id',
         '3DBall',
         '--inference',
@@ -290,7 +417,7 @@ def test_resume_override_configuration_values(grim_config):
     """Test for correct handling of the '--resume' argument."""
 
     # We test handling of the '--resume' argument by itself as it will affect the result
-    # of the timestamps and inference arguments.
+    # of the timestamp and inference arguments.
 
     override_args = Namespace(
         configuration_file='config/3DBall_grimagents.json',
@@ -320,16 +447,13 @@ def test_resume_override_configuration_values(grim_config):
     # The absolute path to training_wrapper.py will differ based on the system running this test.
     result[3] = 'grimagents/training_wrapper.py'
 
+    # The inference argument should not be present and a timestamp should not be applied.
     assert result == [
         'pipenv',
         'run',
         'python',
         'grimagents/training_wrapper.py',
         'config/3DBall.yaml',
-        '--env',
-        'builds/3DBall/3DBall.exe',
-        '--export-path',
-        'UnitySDK/Assets/ML-Agents/Examples/3DBall/ImportedModels',
         '--run-id',
         '3DBall',
         '--resume',
@@ -361,57 +485,6 @@ def test_training_arguments_set_methods(grim_config):
     assert '--run-id ball-' in arguments_string
     assert '--multi-gpu' in arguments_string
     assert '--num-orcs 42' in arguments_string
-
-
-def test_training_arguments_timestamp(monkeypatch, grim_config):
-    """Test for TrainingWrapperArguments correctly applying a timestamp."""
-
-    def mock_return():
-        return '2019-06-29_17-13-41'
-
-    monkeypatch.setattr(grimagents.common, "get_timestamp", mock_return)
-
-    grim_config['--timestamp'] = True
-    arguments = TrainingWrapperArguments(grim_config)
-    arguments_string = arguments.get_arguments_as_string()
-
-    assert '--run-id 3DBall-2019-06-29_17-13-41' in arguments_string
-
-
-def test_training_arguments_inference(grim_config):
-    """Tests for correct processing of the '--inference' argument.
-
-    - Ensures '--inference' argument is added
-    - Ensures get_arguments() can handle no additional args being set"
-    - Ensures the '--export-path' argument is removed
-    """
-
-    # '--inference' should not be present
-    grim_config['--inference'] = False
-    arguments = TrainingWrapperArguments(grim_config)
-    assert '--inference' not in arguments.get_arguments()
-
-    # '--inference' should be present and '--export-path' argument should be removed
-    grim_config['--inference'] = True
-    arguments = TrainingWrapperArguments(grim_config)
-    arguments_list = [
-        'pipenv',
-        'run',
-        'python',
-        'grimagents/training_wrapper.py',
-        'config/3DBall.yaml',
-        '--env',
-        'builds/3DBall/3DBall.exe',
-        '--run-id',
-        '3DBall',
-        '--inference',
-    ]
-
-    # The absolute path to training_wrapper.py will differ based on the system running this test.
-    result = arguments.get_arguments()
-    result[3] = 'grimagents/training_wrapper.py'
-
-    assert result == arguments_list
 
 
 def test_env_args_placed_last():
