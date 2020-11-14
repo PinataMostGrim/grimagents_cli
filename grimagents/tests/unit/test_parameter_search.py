c72b7cbd4bb98475d1d9c04e8dc8590cb6c26293
Aaron <pinatamostgrim@gmail.com>
2020-11-14 12:17:52 -0400

    Update unit tests
    Update unit tests to account for the new trainer configuration structure.

--- grimagents/tests/unit/test_parameter_search.py
+++ grimagents/tests/unit/test_parameter_search.py
@@ -5,7 +5,6 @@ from grimagents.parameter_search import (
     GridSearch,
     RandomSearch,
     BayesianSearch,
-    InvalidTrainerConfig,
     InvalidGridSearchIndex,
 )
 
@@ -13,34 +12,49 @@ from grimagents.parameter_search import (
 @pytest.fixture
 def search_config():
     return {
-        'brain': {
-            'name': 'BRAIN_NAME',
-            'hyperparameters': {
-                'beta': [1e-4, 1e-2],
-                'hidden_units': [32, 512],
-                'learning_rate': [1e-5, 1e-3],
-                'num_layers': [1, 3],
-                'num_epoch': [3, 10],
-            },
-        }
+        'behavior_name': 'BEHAVIOR_NAME',
+        'search_parameters': {
+            'hyperparameters.beta': [1e-4, 1e-2],
+            'hyperparameters.num_epoch': [3, 10],
+            'hyperparameters.learning_rate': [1e-5, 1e-3],
+            'network_settings.hidden_units': [32, 512],
+            'network_settings.num_layers': [1, 3],
+        },
     }
 
 
 @pytest.fixture
 def trainer_config():
     return {
-        'default': {
-            'trainer': 'ppo',
-            'batch_size': 1024,
-            'beta': 5.0e-3,
-            'buffer_size': 10240,
-            'epsilon': 0.2,
-            'gamma': 0.99,
-            'hidden_units': 128,
-            'lambd': 0.95,
+        'behaviors': {
+            'BEHAVIOR_NAME': {
+                'trainer_type': 'ppo',
+                'hyperparameters': {
+                    'batch_size': 1024,
+                    'beta': 5.0e-3,
+                    'buffer_size': 10240,
+                    'epsilon': 0.2,
+                    'lambd': 0.95,
+                },
+                'network_settings': {
+                    'hidden_units': 128,
+                },
+                'reward_signals': {
+                    'extrinsic': {
+                        'gamma': 0.99,
+                        'strength': 1.0,
+                    },
+                },
+            },
+            'OTHER_BEHAVIOR_NAME': {
+                'trainer_type': 'ppo',
+                'hyperparameters': {
+                    'batch_size': 1024,
+                    'buffer_size': 10240,
+                    'epsilon': 0.2,
+                },
+            },
         },
-        'BRAIN_NAME': {'beta': 5.0e-3, 'epsilon': 0.2},
-        'OTHER_BRAIN_NAME': {'batch_size': 1024, 'buffer_size': 10240, 'epsilon': 0.2},
     }
 
 
@@ -48,11 +62,11 @@ def test_get_search_hyperparameters(search_config):
     """Test for the extraction of hyperparameter names from a search configuration dictionary."""
 
     assert GridSearch.get_search_hyperparameters(search_config) == [
-        'beta',
-        'hidden_units',
-        'learning_rate',
-        'num_layers',
-        'num_epoch',
+        'hyperparameters.beta',
+        'hyperparameters.num_epoch',
+        'hyperparameters.learning_rate',
+        'network_settings.hidden_units',
+        'network_settings.num_layers',
     ]
 
 
@@ -61,10 +75,10 @@ def test_get_hyperparameter_sets(search_config):
 
     assert GridSearch.get_hyperparameter_sets(search_config) == [
         [0.0001, 0.01],
-        [32, 512],
-        [1e-05, 0.001],
-        [1, 3],
         [3, 10],
+        [1e-05, 0.001],
+        [32, 512],
+        [1, 3],
     ]
 
 
@@ -84,51 +98,33 @@ def test_get_search_permutations(search_config):
     ]
 
 
-def test_extract_brain_config(trainer_config):
-    """Tests for the correct creation of the GridSearch brain configuration from a trainer configuration."""
-
-    assert GridSearch.extract_brain_config(trainer_config, 'BRAIN_NAME') == {
-        'default': {
-            'trainer': 'ppo',
-            'batch_size': 1024,
-            'beta': 0.005,
-            'buffer_size': 10240,
-            'epsilon': 0.2,
-            'gamma': 0.99,
-            'hidden_units': 128,
-            'lambd': 0.95,
-        },
-        'BRAIN_NAME': {'beta': 0.005, 'epsilon': 0.2},
-    }
-
-
-def test_get_search_configuration(search_config, trainer_config):
-    """Tests for the correct creation of GridSearch intersect configurations."""
+def test_get_grid_search_configuration(search_config, trainer_config):
+    """Tests for the correct creation of GridSearch configurations."""
 
     search = GridSearch(search_config, trainer_config)
 
     assert search.get_search_configuration(0) == {
-        'beta': 0.0001,
-        'hidden_units': 32,
-        'learning_rate': 1e-05,
-        'num_layers': 1,
-        'num_epoch': 3,
+        'hyperparameters.beta': 0.0001,
+        'hyperparameters.learning_rate': 1e-05,
+        'hyperparameters.num_epoch': 3,
+        'network_settings.hidden_units': 32,
+        'network_settings.num_layers': 1,
     }
 
     assert search.get_search_configuration(15) == {
-        'beta': 0.0001,
-        'hidden_units': 512,
-        'learning_rate': 0.001,
-        'num_layers': 3,
-        'num_epoch': 10,
+        'hyperparameters.beta': 0.0001,
+        'hyperparameters.learning_rate': 0.001,
+        'hyperparameters.num_epoch': 10,
+        'network_settings.hidden_units': 512,
+        'network_settings.num_layers': 3,
     }
 
     assert search.get_search_configuration(31) == {
-        'beta': 0.01,
-        'hidden_units': 512,
-        'learning_rate': 0.001,
-        'num_layers': 3,
-        'num_epoch': 10,
+        'hyperparameters.beta': 0.01,
+        'hyperparameters.learning_rate': 0.001,
+        'hyperparameters.num_epoch': 10,
+        'network_settings.hidden_units': 512,
+        'network_settings.num_layers': 3,
     }
 
 
@@ -148,111 +144,76 @@ def test_invalid_grid_search_index(search_config, trainer_config):
         search.get_search_configuration(32)
 
 
-def test_invalid_trainer_config(trainer_config):
-    """Test for the correct raising of InvalidTrainerConfig exceptions."""
-
-    # Test for configuration missing 'brain_name'
-    with pytest.raises(InvalidTrainerConfig):
-        assert GridSearch.extract_brain_config(trainer_config, 'DOES_NOT_EXIST') == {
-            'default': {
-                'trainer': 'ppo',
-                'batch_size': 1024,
-                'beta': 0.005,
-                'buffer_size': 10240,
-                'epsilon': 0.2,
-                'gamma': 0.99,
-                'hidden_units': 128,
-                'lambd': 0.95,
-            },
-            'BRAIN_NAME': {'beta': 0.005, 'epsilon': 0.2},
-        }
-
-    # Test for configuration missing 'default'
-    del trainer_config['default']
-    with pytest.raises(InvalidTrainerConfig):
-        assert GridSearch.extract_brain_config(trainer_config, 'BRAIN_NAME') == {
-            'default': {
-                'trainer': 'ppo',
-                'batch_size': 1024,
-                'beta': 0.005,
-                'buffer_size': 10240,
-                'epsilon': 0.2,
-                'gamma': 0.99,
-                'hidden_units': 128,
-                'lambd': 0.95,
-            },
-            'BRAIN_NAME': {'beta': 0.005, 'epsilon': 0.2},
-        }
-
-
-def test_get_brain_config_with_overrides(search_config, trainer_config):
-    """Tests that flattened reward_signal keys are correctly expanded for the brain configuration. Additionally tests to ensure nested sibling keys do not get overwritten."""
-
-    # Insert am unrelated reward signal to ensure it does not get overwritten by
-    # the overrides.
-    trainer_config['BRAIN_NAME']['reward_signal'] = {'extrinsic': {'strength': 1.0}}
+def test_get_trainer_config_with_overrides(search_config, trainer_config):
+    """Tests that flattened period separated keys are correctly expanded for the search configuration. Additionally tests to ensure nested sibling keys do not get overwritten."""
 
     search = GridSearch(search_config, trainer_config)
     overrides = {
-        'reward_signal.extrinsic.gamma': 0.99,
-        'reward_signal.curiosity.curiosity_enc_size': 256,
+        'reward_signals.extrinsic.gamma': 0.91,
+        'reward_signals.curiosity.encoding_size': 256,
+        'reward_signals.curiosity.strength': 1.0,
+        'reward_signals.curiosity.gamma': 0.99,
     }
 
-    brain_config = search.get_trainer_config_with_overrides(overrides)
+    search_config = search.get_trainer_config_with_overrides(overrides)
 
-    assert brain_config == {
-        'BRAIN_NAME': {
-            'beta': 0.005,
-            'epsilon': 0.2,
-            'reward_signal': {
-                'curiosity': {'curiosity_enc_size': 256},
-                'extrinsic': {'gamma': 0.99, 'strength': 1.0},
+    assert search_config == {
+        'behaviors': {
+            'BEHAVIOR_NAME': {
+                'hyperparameters': {
+                    'batch_size': 1024,
+                    'beta': 0.005,
+                    'buffer_size': 10240,
+                    'epsilon': 0.2,
+                    'lambd': 0.95,
+                },
+                'network_settings': {'hidden_units': 128},
+                'reward_signals': {
+                    'curiosity': {'encoding_size': 256, 'gamma': 0.99, 'strength': 1.0},
+                    'extrinsic': {'gamma': 0.91, 'strength': 1.0},
+                },
+                'trainer_type': 'ppo',
             },
-        },
-        'default': {
-            'batch_size': 1024,
-            'beta': 0.005,
-            'buffer_size': 10240,
-            'epsilon': 0.2,
-            'gamma': 0.99,
-            'hidden_units': 128,
-            'lambd': 0.95,
-            'trainer': 'ppo',
-        },
+            'OTHER_BEHAVIOR_NAME': {
+                'hyperparameters': {'batch_size': 1024, 'buffer_size': 10240, 'epsilon': 0.2},
+                'trainer_type': 'ppo',
+            },
+        }
     }
 
 
 def test_buffer_size_multiple(search_config, trainer_config):
     """Tests that 'buffer_size' is correctly calculated if 'buffer_size_multiple' is present and that 'buffer_size_multiple' is stripped from the brain_config."""
 
-    search_config['brain']['hyperparameters']['buffer_size_multiple'] = [4]
+    search_config['search_parameters']['hyperparameters.buffer_size_multiple'] = [4]
 
     search = GridSearch(search_config, trainer_config)
-    overrides = search.get_search_configuration(0)
-    brain_config = search.get_trainer_config_with_overrides(overrides)
+    search_overrides = search.get_search_configuration(0)
+    trainer_config = search.get_trainer_config_with_overrides(search_overrides)
 
-    assert 'buffer_size_multiple' not in brain_config['BRAIN_NAME']
+    assert 'buffer_size_multiple' not in trainer_config['behaviors']['BEHAVIOR_NAME']
 
-    assert brain_config == {
-        'default': {
-            'trainer': 'ppo',
-            'batch_size': 1024,
-            'beta': 0.005,
-            'buffer_size': 10240,
-            'epsilon': 0.2,
-            'gamma': 0.99,
-            'hidden_units': 128,
-            'lambd': 0.95,
-        },
-        'BRAIN_NAME': {
-            'beta': 0.0001,
-            'epsilon': 0.2,
-            'hidden_units': 32,
-            'learning_rate': 1e-05,
-            'num_layers': 1,
-            'num_epoch': 3,
-            'buffer_size': 4096,
-        },
+    assert trainer_config == {
+        'behaviors': {
+            'BEHAVIOR_NAME': {
+                'hyperparameters': {
+                    'batch_size': 1024,
+                    'beta': 0.0001,
+                    'buffer_size': 4096,
+                    'epsilon': 0.2,
+                    'lambd': 0.95,
+                    'learning_rate': 1e-05,
+                    'num_epoch': 3,
+                },
+                'network_settings': {'hidden_units': 32, 'num_layers': 1},
+                'reward_signals': {'extrinsic': {'gamma': 0.99, 'strength': 1.0}},
+                'trainer_type': 'ppo',
+            },
+            'OTHER_BEHAVIOR_NAME': {
+                'hyperparameters': {'batch_size': 1024, 'buffer_size': 10240, 'epsilon': 0.2},
+                'trainer_type': 'ppo',
+            },
+        }
     }
 
 
@@ -263,18 +224,18 @@ def test_get_random_value():
     assert RandomSearch.get_random_value([0.01, 0.0001, 1], seed=5) == 0.6229394047202129
 
 
-def test_get_random_intersect(search_config, trainer_config):
-    """Tests for the correct generation of a randomized intersect."""
+def test_get_random_search(search_config, trainer_config):
+    """Tests for the correct generation of a randomized search configuration."""
 
     search = RandomSearch(search_config, trainer_config)
-    random_intersect = search.get_randomized_search_configuration(seed=9871237)
+    random_search_config = search.get_randomized_search_configuration(seed=9871237)
 
-    assert random_intersect == {
-        'beta': 0.008715030393329336,
-        'hidden_units': 477,
-        'learning_rate': 0.0008715030393329336,
-        'num_layers': 1,
-        'num_epoch': 4,
+    assert random_search_config == {
+        'hyperparameters.beta': 0.008715030393329336,
+        'hyperparameters.learning_rate': 0.0008715030393329336,
+        'hyperparameters.num_epoch': 4,
+        'network_settings.hidden_units': 477,
+        'network_settings.num_layers': 1,
     }
 
 
@@ -305,35 +266,35 @@ def test_get_search_config_from_bounds():
     """
 
     bounds = {
-        'batch_size': numpy.float64(144.0682249028942),
-        'beta': numpy.float64(0.0028687875149226343),
-        'buffer_size_multiple': numpy.float64(50.017156222601734),
-        'hidden_units': numpy.float64(121.0682249028942),
-        'num_epoch': numpy.float64(5.028942),
-        'max_steps': numpy.float64(500000.1),
-        'num_layers': numpy.float64(2.49028942),
-        'time_horizon': numpy.float64(64.049292),
-        'sequence_length': numpy.float64(144.9028),
-        'memory_size': numpy.float64(154.9019),
+        'hyperparameters.batch_size': numpy.float64(144.0682249028942),
+        'hyperparameters.beta': numpy.float64(0.0028687875149226343),
+        'hyperparameters.buffer_size_multiple': numpy.float64(50.017156222601734),
+        'hyperparameters.num_epoch': numpy.float64(5.028942),
+        'network_settings.hidden_units': numpy.float64(121.0682249028942),
+        'network_settings.num_layers': numpy.float64(2.49028942),
+        'network_settings.memory.memory_size': numpy.float64(154.9019),
+        'network_settings.memory.sequence_length': numpy.float64(144.9028),
         'reward_signal.extrinsic.strength': numpy.float64(1.0),
         'reward_signal.strength.encoding_size': numpy.float64(184.6824928),
         'reward_signal.curiosity.encoding_size': numpy.float64(184.6824928),
         'reward_signal.gail.encoding_size': numpy.float64(184.6824928),
+        'max_steps': numpy.float64(500000.1),
+        'time_horizon': numpy.float64(64.049292),
     }
 
     assert BayesianSearch.get_search_config_from_bounds(bounds) == {
-        'batch_size': 144,
-        'beta': 0.0028687875149226343,
-        'buffer_size_multiple': 50,
-        'hidden_units': 121,
-        'num_epoch': 5,
+        'hyperparameters.batch_size': 144,
+        'hyperparameters.beta': 0.0028687875149226343,
+        'hyperparameters.buffer_size_multiple': 50,
+        'hyperparameters.num_epoch': 5,
         'max_steps': 500000,
-        'num_layers': 2,
-        'time_horizon': 64,
-        'sequence_length': 145,
-        'memory_size': 152,
-        'reward_signal.extrinsic.strength': 1.0,
-        'reward_signal.strength.encoding_size': 185,
+        'network_settings.hidden_units': 121,
+        'network_settings.memory.memory_size': 152,
+        'network_settings.memory.sequence_length': 145,
+        'network_settings.num_layers': 2,
         'reward_signal.curiosity.encoding_size': 185,
+        'reward_signal.extrinsic.strength': 1.0,
         'reward_signal.gail.encoding_size': 185,
+        'reward_signal.strength.encoding_size': 185,
+        'time_horizon': 64,
     }
